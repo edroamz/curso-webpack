1aaaa3a814130da4b52263b81c1f40e1df28dc8f
edroamz <eduardo.roamz@gmail.com>
2021-04-13 20:13:17 -0500

    Se incluyen nuevos plugins. Se agrega configuración de webpack para dev y se optimiza para producción. Se crean archivos de configuración para netlify y babel. Se definen variables de entorno

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import Template from './templates/Template.js';
-console.log('hola');
+import Template from "@templates/Template.js";
+import "@styles/main.css";
+import "@styles/var.styl";
+import "@styles/var.scss";
 
 (async function App() {
-  const main = null || document.getElementById('main');
+  const main = null || document.getElementById("main");
   main.innerHTML = await Template();
 })();
